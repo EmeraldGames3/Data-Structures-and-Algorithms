1d21d5a6758dcc36c6b1cc2bddc3bd29d82523c0
Bogdan <bobo.oprisiu@gmail.com>
2023-03-25 19:46:06 +0200

    Added SetUp for maze solving

---
+++ Labor/labor2/Robot/RobotSetup.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bogdan on 25.03.2023.
+//
+
+#include "RobotSetup.h"
---
+++ Labor/labor2/Robot/RobotSetup.h
@@ -0,0 +1,8 @@
+//
+// Created by bogdan on 25.03.2023.
+//
+
+#ifndef LABOR2_ROBOTSETUP_H
+#define LABOR2_ROBOTSETUP_H
+
+#endif //LABOR2_ROBOTSETUP_H
