02e3577f60f832ecaca59c6c1d3772295b1c61d4
Bogdan <bobo.oprisiu@gmail.com>
2023-05-03 21:08:15 +0300

    .

--- Labor/Labor3/Tests/test.cpp
+++ Labor/Labor3/Tests/test.cpp
@@ -1,7 +1,10 @@
 #include "test.h"
+#include <iostream>
 
 void test(){
+    std::cout << "Test Queue:\n";
     testAll();
     testAllExtended();
+    std::cout << "\nTest Stack:\n";
     testStack();
 }
